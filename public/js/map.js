22057c3b92f6c16d5656e9461ea1e3928e461f41
Lester <67484367+Lester-Bassantes@users.noreply.github.com>
2024-10-09 15:13:38 -0500

    [UPDATE]

--- public/js/map.js
+++ public/js/map.js
@@ -1,29 +1,24 @@
 // Espera a que el documento esté completamente cargado
 document.addEventListener('DOMContentLoaded', function () {
-  // Configura la ubicación del local
-  let location = [-3.2501927438742073, -79.9724507162064]; // Tu ubicación
+  if (document.getElementById('map')) {
+    // Configura la ubicación del local
+    let location = [-3.2501927438742073, -79.9724507162064];
 
-  // Inicializa el mapa
-  let map = L.map('map').setView(location, 13);
+    let map = L.map('map').setView(location, 13);
 
-  // Agrega la capa de teselas (tiles)
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  }).addTo(map);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
 
-  // Agrega un marcador a la ubicación
-  let marker = L.marker(location).addTo(map);
+    let marker = L.marker(location).addTo(map);
 
-  // Crea el popup y asígnalo al marcador
-  let popup = L.popup()
-    .setContent("Nos encontramos aquí");
+    let popup = L.popup()
+      .setContent("Nos encontramos aquí");
 
-  // Asocia el popup al marcador
-  marker.bindPopup(popup);
-
-  // Abre el popup al cargar el mapa
-  marker.openPopup();
+    marker.bindPopup(popup);
+    marker.openPopup();
+  }
 });
 
 
